e5027842cbc48dc28d780858ee278bfc4761d6a6
Piter1210 <a3522110732@alumno.uttehuacan.edu.mx>
2025-07-18 16:57:21 -0600

    Fix Jest config for ESM support

---
+++ run.test.js
@@ -0,0 +1,11 @@
+// run-tests.js
+import { spawn } from 'node:child_process';
+
+const jest = spawn('node', [
+  '--experimental-vm-modules',
+  './node_modules/jest/bin/jest.js'
+], { stdio: 'inherit' });
+
+jest.on('close', (code) => {
+  process.exit(code);
+});
